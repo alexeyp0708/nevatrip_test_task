842c5a7dc98fa92d07660666fea876452c501f61
Alexey <alexeyp0708@gmail.com>
2024-11-04 19:47:11 +0500

    work day

--- task1/src/Controller/ValidateRequestTrait.php
+++ task1/src/Controller/ValidateRequestTrait.php
@@ -7,13 +7,6 @@ namespace App\Controller;
  */
 trait ValidateRequestTrait
 {
-    /**
-     * Checks the presence of the required parameters
-     * @param array $keys
-     * @param array $checkKeys
-     * @param bool $use_strict
-     * @return bool
-     */
     private function checkRequestKeys(array $keys, array $checkKeys, bool $use_strict = true): bool
     {
         if ($use_strict && count($keys) !== count($checkKeys)) {
